50de14ffab83cd9f6f6ab7b2f7c9d3a2d4cc93c7
William Boxhall <klaymen@Williams-MacBook-Air.local>
2012-11-07 20:08:29 +1100

    return 400 BAD_REQUEST when validation fails

--- src/main/java/org/example/presentation/ValidationHandlerExceptionResolver.java
+++ src/main/java/org/example/presentation/ValidationHandlerExceptionResolver.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.sf.oval.exception.ConstraintsViolatedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,6 +17,7 @@ public class ValidationHandlerExceptionResolver implements HandlerExceptionResol
 
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         if (ex instanceof ConstraintsViolatedException) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
             return new ModelAndView().addObject("validationResult", validationExceptionConverter.convert((ConstraintsViolatedException) ex));
         }
         return null;
