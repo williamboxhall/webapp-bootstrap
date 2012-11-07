0e2c790bbaeff6172a4e8daab2504d91e431dc8b
William Boxhall <klaymen@Williams-MacBook-Air.local>
2012-11-07 20:00:18 +1100

    now intercepting validation errors and returning them as json payloads

---
+++ src/main/java/org/example/presentation/ValidationHandlerExceptionResolver.java
@@ -0,0 +1,23 @@
+package org.example.presentation;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.oval.exception.ConstraintsViolatedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class ValidationHandlerExceptionResolver implements HandlerExceptionResolver {
+    @Autowired
+    private ValidationExceptionConverter validationExceptionConverter;
+
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        if (ex instanceof ConstraintsViolatedException) {
+            return new ModelAndView().addObject("validationResult", validationExceptionConverter.convert((ConstraintsViolatedException) ex));
+        }
+        return null;
+    }
+}
